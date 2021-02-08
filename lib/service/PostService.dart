ec0a9d446f0718f227f9e567896d9153ae56f61e
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-02-09 01:08:26 +0300

    getPostByID() method added to post service

--- lib/service/PostService.dart
+++ lib/service/PostService.dart
@@ -8,10 +8,15 @@ import '../Constants.dart';
 class PostService {
 
  static String url = Constants.BASE_URL + "/api/post";
+ static String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTYyMDQwMTU5MiwiaWF0IjoxNjExNzYxNTkyfQ.d54aNmSkqwHK7xQGQZUpalpxOmrozjnMvL5rqmELknI";
 
   static Future<List<Post>> getPosts() async{
     try{
-        final response = await http.get(url);
+      Map<String,String> header={
+        "Authorization":"Bearer "+token
+      };
+
+        final response = await http.get(url,headers: header);
        if(response.statusCode==200){
          List<Post> posts=postsFromJson(response.body);
          print(posts.toString());
@@ -27,5 +32,29 @@ class PostService {
       return List<Post>();
     }
   }
+
+ static Future<Post> getPostByID(int id) async{
+   try{
+     Map<String,String> header={
+       "Authorization":"Bearer "+token
+     };
+     final response = await http.get(url+"/"+id.toString(),headers: header);
+     if(response.statusCode==200){
+      // print(response.body);
+       var jsonResponse = json.decode(response.body);
+       Post post= Post.fromJson(jsonResponse);
+       print(post.detail.toString());
+       return post;
+     }
+     else{
+       print("Hata Oluştu!");
+       return Post();
+     }
+   }
+   catch (e){
+     print("Hata Oluştu!\n"+e.toString());
+     return Post();
+   }
+ }
 
 }
