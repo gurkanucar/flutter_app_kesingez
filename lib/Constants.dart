c45dcc954fab622a4a1af7e014bc2c40ed7dffa2
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-02-09 15:44:08 +0300

    image slider added to post detail page

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -1,4 +1,7 @@
 class Constants{
  static String BASE_URL="http://192.168.0.21:8080";
  static String IMAGE_BASE_URL="/api/files/downloadFile/";
+ static Map REGIONS={"MARMARA":"Marmara","AKDENIZ":"Akdeniz","KARADENIZ":"Karadeniz","EGE":"Ege","IC_ANADOLU":"İç Anadolu","DOGU_ANADOLU":"Doğu Anadolu","GUNEY_DOGU_ANADOLU":"Güneydoğu Anadolu"};
+ static Map COUNTRIES={"TURKEY":"Türkiye"};
+
 }
--- lib/pages/PostDetailPage.dart
+++ lib/pages/PostDetailPage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_kesingez_denemeler/model/Post.dart';
 import 'package:flutter_app_kesingez_denemeler/service/PostService.dart';
@@ -12,13 +13,45 @@ class PostDetailPage extends StatefulWidget {
 class _PostDetailPageState extends State<PostDetailPage> {
   Post _post;
   bool _loading;
+  int _current = 0;
+  List _imgList = [];
+  int _maxLineBeforeTap = 2;
 
-  void getPost() {
+  String _address = "";
+  String _addressDetail = "";
+
+  String makeCapitalLetter(String s) {
+    s = s[0].toUpperCase() + s.substring(1, s.length).toLowerCase();
+    return s;
+  }
+
+  void getPost() async {
     _loading = true;
-    PostService.getPostByID(3).then((value) {
+    await PostService.getPostByID(2).then((value) {
       setState(() {
         _post = value;
-        _loading = false;
+        _post.imageList.forEach((element) {
+          _imgList.add(element.name);
+          print(element.name);
+          _maxLineBeforeTap = 2;
+        });
+
+        if (_post.address.addressDto != null) {
+          _address = Constants
+                  .COUNTRIES[_post.address.addressDto.countryName.toUpperCase()]
+                  .toUpperCase() +
+              ", " +
+              Constants.REGIONS[_post.address.addressDto.region] +
+              ", " +
+              makeCapitalLetter(_post.address.addressDto.cityName) +
+              ", " +
+              makeCapitalLetter(_post.address.addressDto.districtName);
+
+          _addressDetail = _post.address.detail;
+        }
+        if (_post.userName != null) {
+          _loading = false;
+        }
       });
     });
   }
@@ -32,36 +65,27 @@ class _PostDetailPageState extends State<PostDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Center(child: CircularProgressIndicator(),)
-      body: _loading ==
-              Center(
-                child: CircularProgressIndicator(),
-              )
-          ? null
-          : Container(child: customScrollView(_post)),
-    );
+        // Center(child: CircularProgressIndicator(),)
+        body: RefreshIndicator(
+      onRefresh: () async {
+        await getPost();
+      },
+      child: _loading == true
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Container(child: customScrollView()),
+    ));
   }
 
-  CustomScrollView customScrollView(Post post) {
+  CustomScrollView customScrollView() {
     return CustomScrollView(slivers: [
       SliverAppBar(
-        expandedHeight: MediaQuery.of(context).size.height * 0.8,
+        backgroundColor: Colors.white,
+        expandedHeight: MediaQuery.of(context).size.height * 0.6,
         flexibleSpace: new FlexibleSpaceBar(
-          background: post?.imageList?.length != null
-              ? Container(
-                  child: post?.imageList?.length != null
-                      ? FadeInImage.assetNetwork(
-                          placeholder: 'assets/loading.gif',
-                          image: post?.imageList?.length != null
-                              ? Constants.BASE_URL +
-                                  Constants.IMAGE_BASE_URL +
-                                  post?.imageList?.first?.name
-                              : 'assets/loading.gif',
-                          fit: BoxFit.cover,
-                        )
-                      : null,
-                )
-              : Container(),
+          background:
+              _post?.imageList?.length != null ? imageSlider() : Container(),
         ),
       ),
       SliverList(
@@ -70,7 +94,7 @@ class _PostDetailPageState extends State<PostDetailPage> {
               title: Container(
             child: Column(
               children: [
-                item(post),
+                item(),
               ],
             ),
           )),
@@ -79,107 +103,182 @@ class _PostDetailPageState extends State<PostDetailPage> {
     ]);
   }
 
-  Container item(Post post) {
+  Container imageSlider() {
     return Container(
-      margin: EdgeInsets.only(top: 15.0, bottom: 15.0, left: 5.0, right: 5.0),
-      child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          elevation: 10,
-          child: Container(
-            margin:
-                EdgeInsets.only(top: 15.0, bottom: 15.0, left: 5.0, right: 5.0),
-            child: Column(
-              children: [
-                ListTile(
-                  title: Container(
-                      margin: EdgeInsets.only(bottom: 10.0),
-                      child: Row(
-                        children: [
-                          Text(post.address.addressDto.cityName +
-                              ", " +
-                              post.address.addressDto.districtName),
-                          Spacer(),
-                          InkWell(
-                            onTap: () {
-                              print("Menu");
-                            },
-                            child: Icon(Icons.menu),
-                          )
-                        ],
-                      )),
-                  subtitle: Text(
-                    post.detail,
-                    maxLines: 4,
-                    overflow: TextOverflow.ellipsis,
-                  ),
+      margin: EdgeInsets.only(top: 15.0, bottom: 0.0, left: 1.0, right: 1.0),
+      child: CarouselSlider(
+        height: MediaQuery.of(context).size.height * 0.6,
+        initialPage: 0,
+        enlargeCenterPage: true,
+        autoPlay: true,
+        reverse: false,
+        enableInfiniteScroll: true,
+        autoPlayInterval: Duration(seconds: 2),
+        autoPlayAnimationDuration: Duration(milliseconds: 2000),
+        pauseAutoPlayOnTouch: Duration(seconds: 10),
+        scrollDirection: Axis.horizontal,
+        onPageChanged: (index) {
+          setState(() {
+            _current = index;
+          });
+        },
+        items: _imgList.map((imgUrl) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white70,
                 ),
-                post.imageList.length > 0
-                    ? Container(
-                        margin: EdgeInsets.only(
-                            top: 15.0, bottom: 15.0, left: 5.0, right: 5.0),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height * 0.3,
-                          child: post.imageList.length > 0
-                              ? FadeInImage.assetNetwork(
-                                  placeholder: 'assets/loading.gif',
-                                  image: post.imageList.length > 0
-                                      ? Constants.BASE_URL +
-                                          Constants.IMAGE_BASE_URL +
-                                          post.imageList.first?.name
-                                      : 'assets/loading.gif',
-                                  fit: BoxFit.fitWidth,
-                                )
-                              : null,
-                        ))
-                    : Container(),
-                Container(
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      InkWell(
-                        child: post.alreadyLiked == 1
-                            ? Icon(Icons.local_see)
-                            : Icon(Icons.local_see_outlined),
+                child: FadeInImage.assetNetwork(
+                  placeholder: 'assets/loading.gif',
+                  image: _post?.imageList?.length != null
+                      ? Constants.BASE_URL +
+                          Constants.IMAGE_BASE_URL +
+                          imgUrl.toString()
+                      : 'assets/loading.gif',
+                  fit: BoxFit.cover,
+                ),
+              );
+            },
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Container item() {
+    return Container(
+        margin: EdgeInsets.only(top: 15.0, bottom: 15.0, left: 1.0, right: 1.0),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blueAccent,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                        top: 15.0, bottom: 15.0, left: 15.0, right: 15.0),
+                    child: Row(
+                      children: [
+                        InkWell(
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.location_pin,
+                                size: 40,
+                                color: Colors.red,
+                              ),
+                              Text("Haritada aç")
+                            ],
+                          ),
+                          onTap: () {
+                            print("Haritada aça basıldı");
+                          },
+                        ),
+                        Spacer(),
+                        Text(
+                          "Adres",
+                          style: TextStyle(fontSize: 25),
+                          maxLines: _maxLineBeforeTap,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(
+                          top: 15.0, bottom: 15.0, left: 15.0, right: 15.0),
+                      child: InkWell(
                         onTap: () {
                           setState(() {
-                            if (post.alreadyLiked != 1) {
-                              post.likeCount += 1;
-                              post.alreadyLiked = 1;
-                            } else {
-                              post.likeCount -= 1;
-                              post.alreadyLiked = 0;
-                            }
+                            _maxLineBeforeTap = 20;
                           });
-                          print("basild");
                         },
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(post.likeCount.toString()),
-                      Spacer(),
-                      Container(
-                          margin: EdgeInsets.only(right: 10.0),
-                          child: InkWell(
-                            onTap: () {
-                              print("basildi");
-                            },
-                            child: CircleAvatar(
-                              child: Text(
-                                  post.userName[0].toString().toUpperCase()),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              _address,
+                              style: TextStyle(fontSize: 20),
+                              maxLines: _maxLineBeforeTap,
+                              overflow: TextOverflow.ellipsis,
                             ),
-                          )),
-                    ],
-                  ),
-                )
-              ],
+                            Text(
+                              "\n" + _addressDetail,
+                              style: TextStyle(fontSize: 17),
+                              maxLines: _maxLineBeforeTap,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ))
+                ],
+              ),
             ),
-          )),
-    );
+            Container(
+              margin: EdgeInsets.only(top: 15.0),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blueAccent,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        Container(
+                            margin: EdgeInsets.only(
+                                top: 15.0,
+                                bottom: 15.0,
+                                left: 15.0,
+                                right: 1.0),
+                            child: Row(
+                              children: [
+                                CircleAvatar(
+                                  child: Text(_post?.userName[0]
+                                      .toString()
+                                      .toUpperCase()),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(
+                                      top: 15.0,
+                                      bottom: 15.0,
+                                      left: 15.0,
+                                      right: 1.0),
+                                  child: Text(
+                                    makeCapitalLetter(_post?.userName),
+                                  ),
+                                )
+                              ],
+                            )),
+                        Spacer(),
+                        Container(
+                          margin: EdgeInsets.only(
+                              top: 15.0, bottom: 15.0, left: 1.0, right: 15.0),
+                          child: Text("Detaylar",
+                              style: TextStyle(fontSize: 25)),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                        top: 15.0, bottom: 15.0, left: 10.0, right: 10.0),
+                    child: Text("   " + _post.detail,
+                        style: TextStyle(fontSize: 20)),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
